ee2f9c5fe1c2b85c3079272145420c87490343b2
Guido Weksler <39995669+Guidowe@users.noreply.github.com>
2025-05-20 10:55:39 -0300

    agrego codigo e imagen

---
+++ procesa_eph.R
@@ -0,0 +1,8 @@
+library(eph)
+library(tidyverse)
+
+eph_3t2022 <-  eph::get_microdata(year = 2022,trimester = 3,type = "individual")
+
+asalariados_totales <- eph_3t2022  %>%  # EPH
+  filter(ESTADO == 1, CAT_OCUP == 3) %>% 
+  summarise(asalariados = sum(PONDERA))
